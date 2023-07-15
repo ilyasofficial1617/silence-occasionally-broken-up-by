749765a2921548523c4557d53961d82792137d95
ilyasofficial1617 <ilyasofficial1617@gmail.com>
2023-07-15 18:45:28 +0700

    audioplayer playlist

--- src/app/audio-player.tsx
+++ src/app/audio-player.tsx
@@ -1,9 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export interface IAudio {
+  name: string;
+  link: string;
+}
+
 type AudioPlayerProps = {
   className?: string;
+  playlist: IAudio[];
 };
 
-function AudioPlayer({ className }: AudioPlayerProps): JSX.Element {
-  return <div className={className}>hi!</div>;
-}
+const AudioPlayer = ({
+  className = "",
+  playlist,
+}: AudioPlayerProps): JSX.Element => {
+  const [currentAudio, setCurrentAudio] = useState(playlist[0]);
+
+  return (
+    <div className={className}>
+      <audio controls>
+        <source src={currentAudio.link} type="audio/mp3" />
+      </audio>
+    </div>
+  );
+};
 
 export default AudioPlayer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 import Image from 'next/image'
-import AudioPlayer from "./audio-player";
+import AudioPlayer, { IAudio } from "./audio-player";
 
 export default function Home() {
+  const generatePlaylist = () => {
+    let playlist: IAudio[] = [
+      {
+        name: "metal pipe",
+        link: "metal_pipe.mp3",
+      },
+    ];
+    return playlist;
+  };
+
   return (
     <main className="min-h-screen p-24 container">
       <p className="text-9xl font-bold">Silence</p>
       <div>
         <div className="text-xl ml-2">Occasionally broken up by ..</div>
-        <AudioPlayer className="asdf" />
+        <AudioPlayer
+          className="ml-2"
+          playlist={generatePlaylist()}
+        ></AudioPlayer>
       </div>
     </main>
   );
