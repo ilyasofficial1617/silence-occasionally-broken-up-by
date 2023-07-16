162a55402054adfbd4cee539a1e2e09068abf890
ilyasofficial1617 <ilyasofficial1617@gmail.com>
2023-07-17 01:02:02 +0700

    listing sound effect from api

--- src/app/audio-player.tsx
+++ src/app/audio-player.tsx
@@ -1,30 +1,112 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import {
+  ChangeEvent,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 
 export interface IAudio {
   name: string;
   link: string;
 }
 
-type AudioPlayerProps = {
+type MemeAudioPlayerProps = {
   className?: string;
-  playlist: IAudio[];
 };
 
-const AudioPlayer = ({
+const MemeAudioPlayer = ({
   className = "",
-  playlist,
-}: AudioPlayerProps): JSX.Element => {
-  const [currentAudio, setCurrentAudio] = useState(playlist[0]);
+}: MemeAudioPlayerProps): JSX.Element => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [audioLink, setAudioLink] = useState("");
+  const [selectedAudio, setSelectedAudio] = useState("silence");
+
+  const [playlist, setPlaylist] = useState<IAudio[]>([
+    {
+      name: "silence",
+      link: "silence.mp3",
+    },
+  ]);
+
+  // const getPlaylist = async (): IAudio[] => {
+  //   await
+  //   process.env.STORAGE_HOST
+  // };
+
+  interface IAudioInfoResponse {
+    name: string;
+    isFile: boolean;
+  }
+
+  useEffect(() => {
+    // update list of sound effect
+    fetch(process.env.NEXT_PUBLIC_STORAGE_HOST!)
+      .then((res) => res.json())
+      .then((data) => {
+        let newPlaylist: IAudio[] = [];
+        // list, filter it by isFile, map into title and link for playlist
+        data.listing
+          .filter(({ name, isFile }: IAudioInfoResponse) => {
+            return isFile;
+          })
+          .map(({ name, isFile }: IAudioInfoResponse) => {
+            let title = name.split("?")[0];
+            let link = process.env.NEXT_PUBLIC_STORAGE_HOST! + name;
+            let audioInfo: IAudio = { name: title, link: link };
+            newPlaylist.push(audioInfo);
+          });
+        // set the updated playlist
+        setPlaylist(newPlaylist);
+      });
+  }, []);
+
+  useEffect(() => {
+    // when playlist updated from server
+    // update the selected default value
+    setSelectedAudio(playlist[0].link);
+    setAudioLink(playlist[0].link);
+  }, [playlist]);
+
+  const onSelectAudio = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedAudio(event.target.value);
+    setAudioLink(event.target.value);
+  };
+
+  const onTestPlay = () => {
+    audioRef.current!.currentTime = 0;
+    audioRef.current!.play();
+  };
 
   return (
     <div className={className}>
-      <audio controls>
-        <source src={currentAudio.link} type="audio/mp3" />
-      </audio>
+      <audio ref={audioRef} src={audioLink} />
+      <div>
+        <select value={selectedAudio} onChange={onSelectAudio}>
+          {playlist.map(({ name, link }, index) => {
+            return (
+              <option key={index} value={link}>
+                {name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div>
+        volume <input type="range" min="1" max="100" className="slider w-36" />
+      </div>
+      <div>
+        <button
+          className="button rounded-full bg-gray-200 p-5 hover:bg-gray-100 "
+          onClick={onTestPlay}
+        >
+          test
+        </button>
+      </div>
     </div>
   );
 };
 
-export default AudioPlayer;
+export default MemeAudioPlayer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,12 @@
-import Image from 'next/image'
-import AudioPlayer, { IAudio } from "./audio-player";
+import MemeAudioPlayer from "./audio-player";
 
 export default function Home() {
-  const generatePlaylist = () => {
-    let playlist: IAudio[] = [
-      {
-        name: "metal pipe",
-        link: "metal_pipe.mp3",
-      },
-    ];
-    return playlist;
-  };
-
   return (
     <main className="min-h-screen p-24 container">
       <p className="text-9xl font-bold">Silence</p>
       <div>
         <div className="text-xl ml-2">Occasionally broken up by ..</div>
-        <AudioPlayer
-          className="ml-2"
-          playlist={generatePlaylist()}
-        ></AudioPlayer>
+        <MemeAudioPlayer />
       </div>
     </main>
   );
