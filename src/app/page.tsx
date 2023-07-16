a424fee014f94fd294ec5205a4cffaf222a48a43
ilyasofficial1617 <ilyasofficial1617@gmail.com>
2023-07-17 01:03:01 +0700

    tidying up with prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import MemeAudioPlayer from "./audio-player";
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-24 container">
+    <main className="container min-h-screen p-24">
       <p className="text-9xl font-bold">Silence</p>
       <div>
-        <div className="text-xl ml-2">Occasionally broken up by ..</div>
+        <div className="ml-2 text-xl">Occasionally broken up by ..</div>
         <MemeAudioPlayer />
       </div>
     </main>
